4452084b9fec65efb55ef526a1add2bf2674849a
js25s <petrauskas.justas@gmail.com>
2021-10-28 16:20:34 +0200

    home component troubles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import MyNav from "./components/MyNav";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <Router>
-        {/* <MyNav title="Strivestaurant" />
-      <Route exact path="/" render={(routerProps) => <Home {...routerProps} title="Strivestaurant" />} />
-      <Route path="/reservations" component={Reservations} />
-      <Route path="/menu" component={Menu} /> */}
+        <MyNav title="Strivestaurant" />
+        <Route
+          exact
+          path="/"
+          render={(routerProps) => (
+            <Home {...routerProps} title="Strivestaurant" />
+          )}
+        />
+        {/* <Route path="/reservations" component={Reservations} />
+      <Route path="/menu" component={Menu} />  */}
       </Router>
     </div>
   );
